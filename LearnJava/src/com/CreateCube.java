628e33ad8d005b12d176a21ca67b74dd1f06e46a
Almazan, Gilbert <59057047+DOMO-Dom@users.noreply.github.com>
2021-06-20 17:11:36 +0800

    Create CreateCube.java
    You need to implement the createCube method. It should create a 3x3x3 three-dimensional array with the following content:
    
    [0, 1, 2]	[3, 4, 5]	[6, 7, 8]
    [0, 1, 2]	[3, 4, 5]	[6, 7, 8]
    [0, 1, 2]	[3, 4, 5]	[6, 7, 8]
    The elements should be of type int.

---
+++ LearnJava/src/com/CreateCube.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.util.Arrays;
+
+class CreateCube {
+  
+    public static void main(String[] args) {
+      for (int[][] x : createCube()) {
+        for (int[] y : x) {
+          for (int z : y) {
+            System.out.print(z + ' '); 
+          }
+            System.out.print(',');
+        }
+        System.out.println();
+      }
+    }
+  
+    public static int[][][] createCube() {
+        // write your code here
+        int[][][] arr = new int[3][3][3]; 
+        
+        for (int i = 0; i < 3; i++) {            
+            for (int j = 0; j < 3; j++) {
+                for (int k = 0; k < 3; k++) {
+                    arr[i][j][k] = k + (arr.length * j);
+                }
+            }
+        }
+        return arr;
+    }
+}
