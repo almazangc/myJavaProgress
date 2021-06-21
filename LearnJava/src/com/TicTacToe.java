8da2b9e01cc32bf756932d56d5754fb22c246abd
Almazan, Gilbert <gilbertalmazan61@gmail.com>
2021-06-21 11:36:28 +0800

    updates

--- LearnJava/src/com/TicTacToe.java
+++ LearnJava/src/com/TicTacToe.java
@@ -1,13 +1,12 @@
 package com;
-<<<<<<< HEAD
 
 import java.util.Scanner;
 import java.util.regex.*;  
 
 /**
- * Author: ALMAZAN Gilbert 2hours
+ * Author: ALMAZAN Gilbert
  */
-public class TicTacToe {
+public class Main {
  
     //static char [][] TicTacToeGrid = {{'x','o','o'}, {'_', '_', 'o'}, {'o','o','o'}};
 
@@ -205,63 +204,3 @@ class getGameInput {
     }
     
 }
-=======
-import java.util.Scanner;
-
-public class TicTacToe {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // firs input of 9 symbols
-        System.out.print("Enter cells: ");
-        String str1 = scanner.nextLine();
-        char[][] field = new char[3][3];
-        // fill an array in a nested for loop
-        for (int i = 0, k = 0; i < field.length && k < 7; i++, k += 3) {
-            for (int j = 0; j < field[i].length; j++) {
-                field[i][j] = str1.charAt(k + j);
-            }
-        }
-        // first output an array with printf() method
-        String dashes = "---------";
-        System.out.println(dashes);
-        for (char[] chars : field) {
-            System.out.printf("| %c %c %c |\n", chars[0], chars[1], chars[2]);
-        }
-        System.out.println(dashes);
-        // second input of 2 integers
-        System.out.print("Enter the coordinates: ");
-        boolean flag = false;        
-        while (!flag) {
-            String str2 = scanner.next();
-            String str3 = scanner.next();
-            // utf-16 value test - is character a number?
-            if (str2.charAt(0) > 57 || str3.charAt(0) > 57) {
-                System.out.print("You should enter numbers!\nEnter the coordinates: ");
-                continue;
-            }
-            // utf-16 value test - is character in the (1; 3) interval?
-            if ((str2.charAt(0) < 49 || str2.charAt(0) > 51) ||
-                    (str3.charAt(0) < 49 || str3.charAt(0) > 51)) {
-                System.out.print("Coordinates should be from 1 to 3!\nEnter the coordinates: ");
-                continue;
-            }
-            // convert character to integer
-            int x = Integer.parseInt(String.valueOf(str2.charAt(0)));
-            int y = Integer.parseInt(String.valueOf(str3.charAt(0)));
-            // is the sell [x][y] free?
-            if (field[x - 1][y - 1] != '_') {
-                System.out.print("This cell is occupied! Choose another one!\nEnter the coordinates: ");
-            } else {
-                field[x - 1][y - 1] = 'X';
-                flag = true;
-            }
-        }
-        // second output an array
-        System.out.println(dashes);
-        for (char[] chars : field) {
-            System.out.printf("| %c %c %c |\n", chars[0], chars[1], chars[2]);
-        }
-        System.out.println(dashes);           
-    }
-}
->>>>>>> 08c88c2e840ab36f2f7e82ea020b8d37cc2fcd77
