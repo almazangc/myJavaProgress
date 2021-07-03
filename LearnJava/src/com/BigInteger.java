6f2c105da477b95bb558748d396d105f2442ccdb
Almazan, Gilbert <59057047+DOMO-Dom@users.noreply.github.com>
2021-07-03 19:48:37 +0800

    BigInteger

---
+++ LearnJava/src/com/BigInteger.java
@@ -0,0 +1,23 @@
+package com;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+
+class BigInteger {
+    public static void main(String[] args) {
+        // BigInteger is immutable and an object
+        Scanner sc = new Scanner(System.in);
+        BigInteger a = sc.nextBigInteger();
+        BigInteger b = sc.nextBigInteger();
+        BigInteger c = sc.nextBigInteger();
+        BigInteger d = sc.nextBigInteger();
+        
+        BigInteger result = expression(a, b, c, d);
+        System.out.print(result);
+    }
+    
+    static BigInteger expression(BigInteger a, BigInteger b, BigInteger c, BigInteger d) {
+        return  a.negate().multiply(b).add(c).subtract(d);
+        //(-a) * b + c - d
+    }
+}
