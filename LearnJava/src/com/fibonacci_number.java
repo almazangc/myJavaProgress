1cad32975204b0c1311c91885331b11576210442
Almazan, Gilbert <59057047+DOMO-Dom@users.noreply.github.com>
2021-10-28 09:27:31 +0800

    Data type change
    *integer to long
    *possible change into big integer for larger values

--- LearnJava/src/com/fibonacci_number.java
+++ LearnJava/src/com/fibonacci_number.java
@@ -1,31 +1,33 @@
 package com;
-import java.util.Scanner;
 
 /**
- * Fibonacci Number - This program generates and display the first n fibonacci number
+ * Fibonacci Number - This program display the first n fibonacci number
  *
  * @author  ALMAZAN, Gilbert C. & ROSAS, Ivan Jomil M.
  * @date   2021-10-26
  */
 
-public class fibonacci_number {
+import java.util.Scanner;
+
+public class fibonacci_sequence {
 
     private static final Scanner scan = new Scanner(System.in);
-    private static int n;
 
-    public static void main(String args[]) {
-        System.out.print("Enter a number: ?"); n = scan.nextInt(); //ask user
+    public static void main(String[] args) {
+        System.out.print("Enter a number: ?");
+        long n = scan.nextInt(); //ask user
 
         System.out.printf("%s%d%s","The first ", n, " Fibonacci numbers are: ");
-        for(int i = 1; i < n + 1; i++){
+        for(long i = 1; i < n + 1; i++){
             System.out.print(recursion(i) +" ");
         }
     }
 
-    private static int recursion(int i) {
+    private static int recursion(long i) {
         if(i == 1 || i == 2){ //Starting number 1 1
             return 1;
         }
         return recursion(i-2) + recursion(i-1); //recursion for next number
     }
 }
+
