825b7b57b03354c58857eb777e100ecb94b84ea6
Gilbert Almazan <DOMO-Dom>
2021-12-19 18:23:30 +0800

    Exception Handling

---
+++ LearnJava/src/com/eHandling.java
@@ -0,0 +1,41 @@
+package com;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class eHandling {
+    static final Scanner getInput = new Scanner(System.in);
+    /*Exception handling and list
+        ArithmeticException
+        ClassNotFoundException
+        IllegalArgumentException
+        IndexOutOfBoundsExceptions
+        InputMismatchException
+        IOException
+     */
+    public static void main(String[] args) {
+        System.out.print("Enter your age: ");
+        int age = getAge();
+
+        if (age != 0 ){
+            System.out.println("You're getting older eh!");
+        }
+}
+
+    public static int getAge(){
+        try{
+            return getInput.nextInt();
+        }
+        catch (InputMismatchException e) {
+            System.out.println("Enter a whole number!!");
+            return 0;
+        }
+        catch (Exception e){
+            System.out.println("Cant accept that!");
+            System.out.println("Error: " + e.toString());
+            System.out.println("Error Message: " + e.getMessage());
+            e.printStackTrace();
+            getInput.next(); //flushing current input
+            return 0;
+        }
+    }
+}
