72a3c4cbd5bb5ed4b93757281a89ca1c0fe683a1
Gilbert Almazan <DOMO-Dom>
2021-12-20 13:53:53 +0800

    New Stuff
    Primitive Data Types
    Operator
    Operand
    Expressions

---
+++ LearnJava/src/com/Primitive_Data_Types.java
@@ -0,0 +1,111 @@
+package com;
+
+public class Primitive_Data_Types {
+    public static void main(String[] args) {
+        // byte
+        // short
+        // int
+        // long
+        // float
+        // double
+        // char
+        // boolean
+        // String
+
+        int myMinIntValue = Integer.MIN_VALUE;
+        int myMaxIntValue = Integer.MAX_VALUE;
+        System.out.println("Integer Minimum Value = " + myMinIntValue);
+        System.out.println("Integer Maximum Value = " + myMaxIntValue);
+        System.out.println("Busted MAX value = " + (myMaxIntValue + 1));
+        System.out.println("Busted MIN value = " + (myMinIntValue - 1));
+
+        //int myMaxIntTest = 2_147_483_647;
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimum Value = " + myMinByteValue);
+        System.out.println("Byte Maximum Value = " + myMaxByteValue);
+
+        short myMinShortValue = Short.MIN_VALUE;
+        short myMaxShortValue = Short.MAX_VALUE;
+        System.out.println("Short Minimum Value = " + myMinShortValue);
+        System.out.println("Short Maximum Value = " + myMaxShortValue);
+
+        //long myLongValue = 100L;
+        long myMinLongValue = Long.MIN_VALUE;
+        long myMaxLongValue = Long.MAX_VALUE;
+        System.out.println("Long Minimum Value = " + myMinLongValue);
+        System.out.println("Long Maximum Value = " + myMaxLongValue);
+        long bigLongLiteralValue = 2_147_483_647_234L;
+        System.out.println(bigLongLiteralValue);
+
+        float myMinFloatValue = Float.MIN_VALUE;
+        float myMaxFloatValue = Float.MAX_VALUE;
+        System.out.println("Float minimum value = " + myMinFloatValue);
+        System.out.println("Float maximum value = " +myMaxFloatValue);
+
+        double myMinDoubleValue = Double.MIN_VALUE;
+        double myMaxDoubleValue = Double.MAX_VALUE;
+        System.out.println("Double minimum value = " + myMinDoubleValue);
+        System.out.println("Double maximum value = " +myMaxDoubleValue);
+
+        //int myIntValue = 5;
+        //float myFloatValue = 5.25f;
+        //double myDoubleValue = 5.25d;
+
+        //short bigShortLiteralValue = 32767;
+
+        //using min and max value method
+        //to prevent under loading and overloading data types
+        //Can use _ to write numbers
+
+
+        //Casting Data Types
+        //int myTotal = (myMinIntValue / 2);
+
+        byte myNewByteValue = (byte) (myMinByteValue / 2);
+        System.out.println(myNewByteValue);
+        short myNewShortValue = (short) (myMinShortValue / 2);
+        System.out.println(myNewShortValue);
+
+        //Auto IDE Type Casting only if the data type for string is larger than actual data
+        byte byteValue = 10;
+        short shortValue = 20;
+        int intValue = 50;
+
+        long longTotal = 50000L + 10L * (byteValue + shortValue + intValue);
+        System.out.println(longTotal);
+
+        short shortTotal = (short) (1000 + 10 *
+                (byteValue + shortValue + intValue));
+        System.out.println(shortTotal);
+
+
+        char myChar = 'D';
+        char myUnicodeChar = '\u0044'; //Use of unicode characters
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+        char myCopyrightChar = '\u00A9';
+        System.out.println(myCopyrightChar);
+        boolean isBool; //use if isVar as naming convention for boolean
+        isBool = true;
+        System.out.println(isBool);
+
+        String myString = "This is a string";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + ", and this is more.";
+        System.out.println("myString is equal to " + myString);
+        myString = myString + " \u00A9 2019";
+        System.out.println("myString is equal to " + myString);
+        String numberString = "250.55";
+        numberString = numberString + "49.95";
+        System.out.println(numberString);
+        String lastString = "10";
+        int myInt = 50;
+        lastString = lastString + myInt;
+        System.out.println("LastString is equal to " + lastString);
+        double doubleNumber = 120.47d;
+        lastString = lastString + doubleNumber;
+        System.out.println("LastString is equal to " + lastString);
+    }
+}
