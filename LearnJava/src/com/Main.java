5d3f9ee0d819ed92f4c95fa606d0886a4c2256e4
Gilbert Almazan <DOMO-Dom>
2021-12-23 11:09:45 +0800

    Coding Excersice
    1 | Speed Converter
    2 | Barking Dog
    3 | Mega Bytes Converter
    4 | Decimal Comparator
    5 | Equal Sum Checker
    6 | Teen Number Checker

---
+++ LearnJava/src/com/Main.java
@@ -0,0 +1,7 @@
+package com;
+
+public class Main {
+    //For Testing purposes
+    public static void main(String[] args) {
+    }
+}
---
+++ LearnJava/src/com/MegaBytesConverter.java
@@ -0,0 +1,13 @@
+package com;
+
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        } else {
+            int megaBytes = kiloBytes / 1024;
+            int remainingMegaBytes = kiloBytes % 1024;
+            System.out.println(kiloBytes + " KB = " + megaBytes + " MB and " + remainingMegaBytes + " KB" );
+        }
+    }
+}
